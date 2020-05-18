43354aed972cbde014a28878dd25d89a0bbca608
Abhinil Agarwal <abhinilagarwal@Abhinils-MacBook-Air.local>
2020-05-19 03:14:23 +0530

    trying diagflow

--- index.js
+++ index.js
@@ -67,13 +67,35 @@ app.get('/webhook', (req, res) => {
 
 
 
-var admin = require("firebase-admin");
-
-var serviceAccount = require("/Users/abhinilagarwal/desktop/messenger-webhook/my-project-97974-firebase-adminsdk-hlc0d-a581d49fae.json");
+//trying something for firebase
+const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true});
+const admin = require('firebase-admin');
+const serviceAccount = require('/Users/abhinilagarwal/desktop/messenger-webhook/my-project-97974-firebase-adminsdk-hlc0d-a581d49fae.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://my-project-97974.firebaseio.com"
+});
+
+const { SessionsClient } = require('dialogflow');
+
+
+exports.dialogflowGateway = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    const { queryInput, sessionId } = request.body;
+
+
+    const sessionClient = new SessionsClient({ credentials: serviceAccount  });
+    const session = sessionClient.sessionPath('My project 97974', sessionId);
+
+
+    const responses = await sessionClient.detectIntent({ session, queryInput});
+
+    const result = responses[0].queryResult;
+
+    response.send(result);
+  });
 });
 
 
