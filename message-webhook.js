09b04c8d7a1ca75d6372a71c67d3bffd062aaee4
Abhinil Agarwal <abhinilagarwal@Abhinils-MacBook-Air.local>
2020-05-20 01:14:50 +0530

    console5

--- message-webhook.js
+++ message-webhook.js
@@ -1,7 +1,9 @@
    const processMessage = require('./process-message');
 
     module.exports = (req, res) => {
+      console.log(req);
       if (req.body.object === 'page') {
+        console.log(req.body.object);
         req.body.entry.forEach(entry => {
           entry.messaging.forEach(event => {
             if (event.message && event.message.text) {
