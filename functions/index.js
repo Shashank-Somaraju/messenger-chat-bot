aef8b9383be66ac049d3a1c4caeed2e8f8b11a2d
Abhinil Agarwal <abhinilagarwal@Abhinils-MacBook-Air.local>
2020-05-19 15:54:32 +0530

    adding functions

---
+++ functions/index.js
@@ -0,0 +1,38 @@
+
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+//trying something for firebase
+const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true});
+const admin = require('firebase-admin');
+const serviceAccount = require('./my-project-97974-firebase-adminsdk-hlc0d-a581d49fae.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://my-project-97974.firebaseio.com"
+});
+
+const { SessionsClient } = require('dialogflow');
+
+
+exports.dialogflowGateway = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+    const { queryInput, sessionId } = request.body;
+
+
+    const sessionClient = new SessionsClient({ credentials: serviceAccount  });
+    const session = sessionClient.sessionPath('my-project-97974', sessionId);
+
+
+    const responses = await sessionClient.detectIntent({ session, queryInput});
+
+    const result = responses[0].queryResult;
+
+    response.send(result);
+  });
+});
--- index.js
+++ index.js
@@ -67,36 +67,6 @@ app.get('/webhook', (req, res) => {
 
 
 
-//trying something for firebase
-const functions = require('firebase-functions');
-const cors = require('cors')({ origin: true});
-const admin = require('firebase-admin');
-const serviceAccount = require('./my-project-97974-firebase-adminsdk-hlc0d-a581d49fae.json');
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://my-project-97974.firebaseio.com"
-});
-
-const { SessionsClient } = require('dialogflow');
-
-
-exports.dialogflowGateway = functions.https.onRequest((request, response) => {
-  cors(request, response, async () => {
-    const { queryInput, sessionId } = request.body;
-
-
-    const sessionClient = new SessionsClient({ credentials: serviceAccount  });
-    const session = sessionClient.sessionPath('my-project-97974', sessionId);
-
-
-    const responses = await sessionClient.detectIntent({ session, queryInput});
-
-    const result = responses[0].queryResult;
-
-    response.send(result);
-  });
-});
 
 
 
