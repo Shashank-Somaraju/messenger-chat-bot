144db894263d064c7975ade6ed9dadd1a1620855
Abhinil Agarwal <abhinilagarwal@Abhinils-MacBook-Air.local>
2020-05-22 04:23:01 +0530

    adding console

--- process-message.js
+++ process-message.js
@@ -66,8 +66,8 @@
         .detectIntent(request)
         .then(responses => {
           const result = responses[0].queryResult;
-          //console.log("BOT BOT BOT:");
-          console.log("Response from BOT => " + result);
+          console.log("BOT BOT BOT:");
+          console.log(result);
           //console.log("END END END");
           console.log(result.fulfillmentText);
           return sendTextMessage(userId, result.fulfillmentText);
