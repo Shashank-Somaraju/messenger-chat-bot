0bc389e3b9fbd2246448cb2f1430524a0489b17f
Abhinil Agarwal <abhinilagarwal@Abhinils-MacBook-Air.local>
2020-05-26 07:41:33 +0530

    adding buttons-adding logs3

--- process-message.js
+++ process-message.js
@@ -75,7 +75,7 @@
 
 
 
-          if(check_if_question(result.fulfillmentText)==true){
+          if(check_if_question(result.fulfillmentText)==1){
             console.log("GOING TO SEND BUTTON");
             return sendButton(userId,["postback","postback","postback","postback"], split_question(result.fulfillmentText), options_array,["Option1","Option2","Option3","Option4"], tall  );
           }
@@ -98,10 +98,10 @@ function check_if_question(text){
     if(text[i]=="?"){
 
       break;
-      return true;
+      return 1;
     }
     else
-      return false;
+      return 0;
   }
 
 }
