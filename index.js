d8ec98cb9d2b7643fb196264e78850bc245fdb2f
Abhinil Agarwal <abhinilagarwal@Abhinils-MacBook-Air.local>
2020-05-26 07:33:53 +0530

    adding buttons-start

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ require('dotenv').config({ path: './variables.env' });
   const request = require('request');
   const verifyWebhook = require('./verify-webhook');
   const messageWebhook = require('./message-webhook');
-  var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div></body></html>";
+  //var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div></body></html>";
   
   const app = express();
 
--- process-message.js
+++ process-message.js
@@ -1,4 +1,5 @@
     const fetch = require('node-fetch');
+    let token ='EAAEinZCXNTnQBACIHFIZBCqKHcZC1gWLMedwjZAPm8KXLBVOMIpl6YfhAmBe2pciwNZA7R6ZApgHzixCKG2IgnaAdo6eM8ZBxFPRANvYHxH1R8ZAnOPhgfNQoUHBD03N7bq0gjrg7Y1BL8DLPeNWjlUaUZBGtr98pE2OZCZA1F4Bk13GwZDZD';
 
     // You can find your project ID in your Dialogflow agent settings
     const projectId = 'my-project-97974'; //https://dialogflow.com/docs/agents#settings
@@ -48,6 +49,7 @@
     module.exports = (event) => {
       const userId = event.sender.id;
       const message = event.message.text;
+
       //console.log("testing 1");
       //console.log(userId);
       //console.log(message);
@@ -70,10 +72,132 @@
           console.log(result);
           //console.log("END END END");
           console.log(result.fulfillmentText);
-          return sendTextMessage(userId, result.fulfillmentText);
+
+
+
+          if(check_if_question(result.fulfillmentText)==true){
+
+            return sendButton(userId,["postback","postback","postback","postback"], split_question(result.fulfillmentText), options_array,["Option1","Option2","Option3","Option4"], tall  );
+          }
+          else
+            return sendTextMessage(userId, result.fulfillmentText);
+
           console.log("TEXT SENT ...");
         })
         .catch(err => {
           console.error('ERROR:', err);
         });
     }
+
+
+function check_if_question(text){
+  for (var i = text.length - 1; i >= 0; i--) {
+    if(text[i]=="?"){
+
+      break;
+      return true;
+    }
+    else
+      return false;
+  }
+
+}
+
+
+var options_array=[];
+
+function split_question(text){
+  var arr = text.split("4.");
+  option_4= arr[1];
+  var arr = arr[0].split("3.");
+  option_3= arr[1];
+  var arr = arr[0].split("2.");
+  option_2= arr[1];
+  var arr = arr[0].split("1.");
+  option_1= arr[1];
+  options_array.push(option_1);
+  options_array.push(option_2);
+  options_array.push(option_3);
+  options_array.push(option_4);
+  question = arr[0];
+  return question;
+}
+
+
+
+
+
+
+
+
+
+
+// figure out parameters
+function sendButton(recipientId,type,text,payload,caption,size){
+  console.log("called")
+        var messageData={
+                recipient:{
+                        id:recipientId
+                },
+                message:{
+                        attachment:{
+                                type:"template",
+                                payload:{
+                                        template_type:"button",
+                                        text:text,
+                                        buttons:[
+                                        ]
+                                }
+                        }
+                }
+        };
+        var filler;
+        for (var i=0;i<type.length;i++)
+        {
+                if (type[i]==="web_url"){
+                        filler ={
+                                type:type[i],
+                                title:caption[i],
+                                url:payload[i],
+                                webview_height_ratio:size
+                        };
+                        messageData.message.attachment.payload.buttons.push(filler);
+                }
+                else if (type[i]==="postback"){
+                        filler ={
+                                type:type[i],
+                                title:caption[i],
+                                payload:payload[i],
+                        };
+                        messageData.message.attachment.payload.buttons.push(filler);
+                }
+        }
+        callSendAPI(messageData);
+}
+
+
+function callSendAPI(messageData) {
+        request({
+                //uri:'https://graph.facebook.com/v7.0/me/messages?access_token=${FACEBOOK_ACCESS_TOKEN}'
+                uri: 'https://graph.facebook.com/v2.6/me/messages',
+                qs: { access_token: token },
+                method: 'POST',
+                json: messageData
+        }, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                        var recipientId = body.recipient_id;
+                        var messageId = body.message_id;
+                        console.log("Successfully sent generic message with id %s to recipient %s",
+                        messageId,recipientId);
+                }
+                else {
+                        //console.error("Unable to send message.");
+                        console.error(response);
+                        //console.error(error);
+                }
+        });
+}
+
+
+
+    
