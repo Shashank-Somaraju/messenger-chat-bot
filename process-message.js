1a8acb92c7603d0e487213ba4b4e45ece6bbdcf9
Abhinil Agarwal <abhinilagarwal@Abhinils-MacBook-Air.local>
2020-05-26 08:33:22 +0530

    adding buttons test again

--- process-message.js
+++ process-message.js
@@ -78,7 +78,7 @@
           if (result.fulfillmentText.includes("Question")){
             console.log("SHOULD SEND BUTTON");
             //return sendButton(userId,["postback","postback","postback","postback"], split_question(result.fulfillmentText), ["Option1","Option2","Option3","Option4"],[options_array[0],options_array[1],options_array[2],options_array[3]], "tall");
-            return sendButton(userId,["postback","postback","postback","postback"], "Test Question",["option1","option2","option_3","option_4"],["Test button1","Test button2","Test 3","Test 4"],"tall");
+            return sendButton(userId,["postback","postback","postback"], "Test Question",["option1","option2","option_3"],["Test button1","Test button2","Test 3"],"tall");
           }
           else{
             console.log("SENDING TEXT");
